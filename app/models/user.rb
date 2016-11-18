bb992ffe59497b263d05ffc410c97e528316e728
Chris <mazurski@my.ccsu.edu>
2016-11-17 22:29:02 -0500

    Implemented Signup,Login/session in progress

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence:true
+  validates :password,confirmation:true
+  has_secure_password
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#login'
+  get '/register', to: 'users#new'
+  get '/login',    to: 'sessions#new' #Page for a new session
+  post '/login',    to: 'sessions#create' #Create a new session
+  delete '/logout', to: 'sessions#destroy' #delete the session
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
