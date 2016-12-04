8c9c8964f486c672bf237ea932f3e3647ca19102
Phu <justinphan16@gmail.com>
2016-12-04 17:31:11 -0500

    Fix a bug: non admin user can't access /user/1 page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     before_action :correct_user,   only: [:edit, :update]
-    before_action :admin_user,     only: :destroy
+    before_action :admin_user,     only: [:destroy, :show]
 
 
     def destroy
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def index
@@ -82,7 +82,10 @@ class UsersController < ApplicationController
     end
 end
 
-def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def admin_user
+        if !(current_user.try(:admin?))
+          flash[:danger] = "You are not authorized to do that."
+          redirect_to(root_url)
+        end
+    end
 end
