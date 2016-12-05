f42569abce18ad534ce65c08874aea76fb435421
Chris <mazurski@my.ccsu.edu>
2016-12-05 01:38:51 -0500

    Modified seeds.rb for the initial products

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,9 @@ User.create!(name:  "adminuser",
              admin: true)
 
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! id: 1, name: "Bananas", price: 1.49, active: true
+Product.create! id: 2, name: "Apples", price: 3.29, active: true
+Product.create! id: 3, name: "Milk", price: 2.99, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
